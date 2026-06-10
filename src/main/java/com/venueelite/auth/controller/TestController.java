443a4bd869c1de55e1729edfca1e8908fd57b866
vimal <vbathumaly@gmail.com>
2026-06-10 16:07:40 +0800

    First commit

--- src/main/java/com/venueelite/auth/controller/TestController.java
+++ src/main/java/com/venueelite/auth/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/test")
 public class TestController {
 
-    @GetMapping("/hello")
+    @GetMapping("/helloworld")
     public String hello() {
         return "Hello, secured API is working!";
     }
