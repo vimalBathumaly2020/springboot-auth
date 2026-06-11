67c1979d36258c03319c9c966d5f4d21c5813a6f
vimal <vbathumaly@gmail.com>
2026-06-11 14:21:54 +0800

    Third commit

---
+++ src/main/java/com/venueelite/auth/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.venueelite.auth.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // Allow all origins
+        config.addAllowedHeader("*");        // Allow all headers
+        config.addAllowedMethod("*");        // Allow all methods (GET, POST, etc.)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
