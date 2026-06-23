0ad5430785d3bbe15c8d66acf33d33bc0e4250ad
vimalBathumaly2020 <151485005+vimalBathumaly2020@users.noreply.github.com>
2026-06-23 17:43:05 +0800

    Update UserController.java

--- src/main/java/com/venueelite/app/controller/UserController.java
+++ src/main/java/com/venueelite/app/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
